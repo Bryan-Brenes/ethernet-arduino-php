3802dd5f828377d362d8e42c8083cc472af39bc6
Bryan <bryanbrenes1@gmail.com>
2019-12-26 15:10:33 -0600

    inicial commit

--- php/save.php
+++ php/save.php
@@ -1,22 +1,29 @@
 <?php
   if(isset($_GET['dato'])){
     echo "Se envió " . $_GET['dato'] . "<br>";
-    // detalles de la conexion
-    $conn_string = "host=ec2-107-22-216-123.compute-1.amazonaws.com port=5432 dbname=dmqe2uft63ru6 user=pdypewzcmiidot password=ddf779b97e2cb698f922969424bc258f4d1649784be568a569a8c2be90ee7a9c options='--client_encoding=UTF8' ssl=true";
-    
-    // establecemos una conexion con el servidor postgresSQL
-    $dbconn = pg_connect($conn_string);
-    
-    // Revisamos el estado de la conexion en caso de errores. 
-    if(!$dbconn) {
-      echo "Error: No se ha podido conectar a la base de datos\n";
-    } else {
-      echo "Conexión exitosa\n";
-    }
- 
-    // Close connection
-    pg_close($dbconn);
+
+    $host = "ec2-107-22-216-123.compute-1.amazonaws.com";
+    $user = "pdypewzcmiidot";
+    $password = "ddf779b97e2cb698f922969424bc258f4d1649784be568a569a8c2be90ee7a9c";
+    $dbname = "dmqe2uft63ru6";
+    $port = "5432";
+
+    //$connStr = "pgsql:host=" . $host . ";port=" . $port . ";dbname=" . $dbname . ";user=" . $user . ";password=" . $password;
+    $conexion = conectar($user, $password, $host, $dbname);
+    pg_query($conexion, "insert into datos(dato) values('" . $_GET['dato'] . "')");
+    echo "Agregado";
+
   } else {
     echo "No se ha enviado un dato correcto";
   }
+
+  function conectar( $usuario, $pass, $host, $bd )
+    {
+      $conexion = pg_connect( "user=".$usuario." ".
+                            "password=".$pass." ".
+                            "host=".$host." ".
+                            "dbname=".$bd
+                            ) or die( "Error al conectar: ".pg_last_error() );
+    return $conexion;
+    }
 ?>
