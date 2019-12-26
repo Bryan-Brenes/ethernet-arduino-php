995c029279f088b64e7ac9e6d7fbc3fe0636d697
Bryan <bryanbrenes1@gmail.com>
2019-12-26 17:02:14 -0600

    new credentials

--- php/save.php
+++ php/save.php
@@ -2,10 +2,10 @@
   if(isset($_GET['dato'])){
     echo "Se envió " . $_GET['dato'] . "<br>";
 
-    $host = "ec2-107-22-216-123.compute-1.amazonaws.com";
-    $user = "pdypewzcmiidot";
-    $password = "ddf779b97e2cb698f922969424bc258f4d1649784be568a569a8c2be90ee7a9c";
-    $dbname = "dmqe2uft63ru6";
+    $host = "ec2-107-22-234-204.compute-1.amazonaws.com";
+    $user = "bzijorxrsqakbv";
+    $password = "b7a96af5c04282ee942f759ca862a1bbdaf05e528fa392eff300e039c98d6358";
+    $dbname = "d4o7frdi5ou1qk";
     $port = "5432";
 
     //$connStr = "pgsql:host=" . $host . ";port=" . $port . ";dbname=" . $dbname . ";user=" . $user . ";password=" . $password;
